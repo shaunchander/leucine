9c87c608b49f47a0c84e3d01f05d25fdee4478c5
shaunchander <shaunchander@gmail.com>
2023-01-29 14:28:14 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+type Levels = "debug" | "info" | "warn" | "error";
+
+const levelIndicators: { [T in Levels]: string } = {
+	debug: `🐛 `,
+	info: `ℹ️  `,
+	warn: `⚠️  `,
+	error: `⛔️ `,
+};
+
+const levelBackgroundColors: { [T in Levels]: string[] } = {
+	debug: ["\x1b[48;5;240m", "darkgray"],
+	info: ["\x1b[48;5;18m", "cornflowerblue"],
+	warn: ["\x1b[48;5;208m", "darkorange"],
+	error: ["\x1b[48;5;88m", "darkred"],
+};
+
+export const log = <T>(arg: T, level: Levels = "debug") => {
+	if (typeof window === "undefined") {
+		console.log(
+			`\x1b[1m${levelBackgroundColors[level][0]}${
+				levelIndicators[level]
+			}(${level}) in ${import.meta.file}\x1b[0m\n\n`,
+			arg,
+			"\n",
+		);
+	} else {
+		console.log(
+			`%c${levelIndicators[level]}(${level})\n\n`,
+			`color:white;background-color:${levelBackgroundColors[level][1]};`,
+			arg,
+			"\n\n",
+		);
+	}
+};
